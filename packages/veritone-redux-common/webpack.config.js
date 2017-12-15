4aa60dec5d1458e03ebfdbae73cdfd305321d963
Mitch Robb <mrobb@veritone.com>
2017-12-15 14:54:03 -0800

    stop-gap: fix up redux-common webpack build

--- packages/veritone-redux-common/webpack.config.js
+++ packages/veritone-redux-common/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+// const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = {
   entry: { 'veritone-redux-common': path.join(__dirname, './src/index.js') },
@@ -9,16 +9,33 @@ module.exports = {
     library: 'veritone-redux-common',
     umdNamedDefine: true
   },
+  externals: {
+    redux: {
+      commonjs: 'redux',
+      commonjs2: 'redux',
+      amd: 'redux'
+    },
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react'
+    },
+    'react-redux': {
+      commonjs: 'react-redux',
+      commonjs2: 'react-redux',
+      amd: 'react-redux'
+    }
+  },
   resolve: {
     alias: {
       helpers: path.join(__dirname, 'src/helpers'),
       modules: path.join(__dirname, 'src/modules'),
-      util: path.join(__dirname, 'src/util'),
-    },
+      util: path.join(__dirname, 'src/util')
+    }
   },
-  plugins: [
-    new MinifyPlugin()
-  ],
+  // plugins: [
+  //   new MinifyPlugin()
+  // ],
   module: {
     // noParse: [],
     rules: [
